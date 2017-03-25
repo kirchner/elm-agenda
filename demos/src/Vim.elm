7dee7c58cf9fbe9d300a4ff177ef036b2501b7fb
Fabian Kirchner <kirchner@posteo.de>
2017-03-25 12:47:24 +0100

    fix vim demo

--- demos/src/Vim.elm
+++ demos/src/Vim.elm
@@ -10,141 +10,70 @@ type Action
     | WriteAll
 
 
-type alias Description =
-    String
+type alias Msg =
+    Char
 
 
-type alias Error =
-    ()
+type alias VimCmd =
+    Agenda () Msg Action
 
 
-everyTool : Agenda (List Description) Char Action Error
-everyTool =
-    let
-        describer maybeDescriptions =
-            case maybeDescriptions of
-                Just descriptions ->
-                    [ "one of: "
-                        ++ (String.join "  or  " descriptions)
-                    ]
-
-                Nothing ->
-                    []
-    in
-        describe describer <|
-            oneOf () [ qTool, wqTool, wallTool ]
-
-
-{-| test for zeroOrMore
--}
-listTool : Agenda Description Char (List ()) Error
-listTool =
-    let
-        describer maybeDescription =
-            case maybeDescription of
-                Just description ->
-                    "zero or more of "
-                        ++ description
-                        ++ " untill '^'"
-
-                Nothing ->
-                    ""
-    in
-        describe describer <|
-            zeroOrMore '^' (tryChar 't')
-
-
-listChainTool : Agenda Description Char (List ()) Error
-listChainTool =
-    let
-        describer maybeDescription =
-            case maybeDescription of
-                Just description ->
-                    "zero or more of "
-                        ++ description
-                        ++ " untill '^'"
-
-                Nothing ->
-                    ""
-    in
-        describe describer <|
-            zeroOrMore '^' (tryChar 't' |.++ tryChar 'c')
+everyCmd : VimCmd
+everyCmd =
+    oneOf [ q, wq, wall ]
 
 
 
 {- tools -}
 
 
-qTool : Agenda Description Char Action Error
-qTool =
-    cmd Quit <|
-        identity
-            |~ tryChar 'q'
+q : VimCmd
+q =
+    cmd Quit (tryChar 'q')
 
 
-wqTool : Agenda Description Char Action Error
-wqTool =
-    cmd WriteQuit <|
-        identity
-            |~ tryChar 'w'
-            |.++ tryChar 'q'
+wq : VimCmd
+wq =
+    cmd WriteQuit
+        (tryChar 'w'
+            >>> tryChar 'q'
+        )
 
 
-wallTool : Agenda Description Char Action Error
-wallTool =
-    cmd WriteAll <|
-        identity
-            |~ tryChar 'w'
-            |.++ tryChar 'a'
-            |.++ tryChar 'l'
-            |.++ tryChar 'l'
+wall : VimCmd
+wall =
+    cmd WriteAll
+        (tryChar 'w'
+            >>> tryChar 'a'
+            >>> tryChar 'l'
+            >>> tryChar 'l'
+        )
 
 
 
 {- helpers -}
 
 
-{-| This is (|=) but also concatenates the descriptions.
--}
-(|.++) :
-    Agenda Description Char keep Error
-    -> Agenda Description Char ignore Error
-    -> Agenda Description Char keep Error
-(|.++) agendaKeep agendaIgnore =
-    let
-        combiner maybeA maybeB =
-            Maybe.withDefault "" <|
-                Maybe.map2 (\a b -> a ++ " and then " ++ b) maybeA maybeB
-    in
-        (agendaKeep |.* combiner) agendaIgnore
-infixl 5 |.++
-
-
 cmd :
     Action
-    -> Agenda Description Char () Error
-    -> Agenda Description Char Action Error
+    -> Agenda () Char ()
+    -> VimCmd
 cmd action agenda =
-    always action
-        |~ colon
-        |.++ agenda
+    colon
+        >>> agenda
+        >>> succeed action
 
 
-colon : Agenda Description Char () Error
+colon : Agenda () Char ()
 colon =
     tryChar ':'
 
 
-tryChar : Char -> Agenda Description Char () Error
+tryChar : Char -> Agenda () Char ()
 tryChar char =
-    let
-        describer =
-            always (toString char)
-
-        cont c =
+    try <|
+        \c ->
             if c == char then
-                Ok ()
+                succeed ()
             else
-                Err ()
-    in
-        describe describer <| try cont
+                fail
